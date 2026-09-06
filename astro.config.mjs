83349bb7b8a2934fa56ddb3b016a1da166564a8d
José Castro <a.j.castro.b@gmail.com>
2026-09-06 15:00:05 -0500

    ✨ [FEAT] Tokens CSS, Fonts API y supersvg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+import supersvgPlugin from "vite-plugin-supersvg";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [supersvgPlugin()],
+  },
+
+  fonts: [
+    {
+      name: "Plus Jakarta Sans",
+      cssVariable: "--font-sans",
+      provider: fontProviders.google(),
+      weights: ["400 800"],
+      styles: ["normal"],
+      fallbacks: ["sans-serif"],
+    },
+    {
+      name: "Space Mono",
+      cssVariable: "--font-mono",
+      provider: fontProviders.google(),
+      weights: [400, 700],
+      styles: ["normal"],
+      fallbacks: ["monospace"],
+    },
+    {
+      name: "Caveat",
+      cssVariable: "--font-hand",
+      provider: fontProviders.google(),
+      weights: [400, 500, 600, 700],
+      styles: ["normal"],
+      fallbacks: ["cursive"],
+    },
+  ],
+});
